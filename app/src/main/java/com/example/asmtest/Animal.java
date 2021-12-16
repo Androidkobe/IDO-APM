e5a7ce2f6db407957ef681a7da537512a87a5b72
sundu <sundu@xiaomi.com>
2021-12-16 15:01:08 +0800

    AspectJ

---
+++ app/src/main/java/com/example/asmtest/Animal.java
@@ -0,0 +1,1 @@
+package com.example.asmtest;import android.util.Log;public class Animal {    public void fly() {        Log.e("sundu", "animal fly method:" + this.toString() + "#fly");    }}
